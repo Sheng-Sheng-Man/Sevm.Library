16ee0d008cca4442ea1afe30705a825301511be4
Conglin Zheng <inmount@live.cn>
2022-08-31 17:12:11 +0800

    refactor: 动态库文件重新命名

---
+++ Sevm.System/Sys.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Sevm.Engine;
+
+namespace Sevm.Library {
+
+    [Script("")]
+    public class Sys {
+
+        /// <summary>
+        /// 输出
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        [Script("获取系统运行毫秒数")]
+        public static Sevm.Engine.Memory.Value GetTickCount(Params args) {
+            return Environment.TickCount;
+        }
+
+    }
+}
