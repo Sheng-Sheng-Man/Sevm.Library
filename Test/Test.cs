3c218f76096a216fc7fb3b6504d7cc2665e4a19e
Conglin Zheng <inmount@live.cn>
2022-09-02 01:56:53 +0800

    feat: 新增了测试库

---
+++ Test/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Sevm.Engine;
+
+namespace Sevm.Library {
+
+    [Script("")]
+    public class Test {
+
+        /// <summary>
+        /// 获取测试
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        [Script("获取测试")]
+        public static Sevm.Engine.Memory.Value GetTickCount(Params args) {
+            return "这是声声慢测试库";
+        }
+
+    }
+}
